64eb44972fd1659d86ccf9c367f20bd477a64459
Ethan McGee <ethan.mcgee@m33integrated.com>
2015-08-28 21:38:16 -0400

    modify // remove users

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 var session = require('express-session')
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+
 
 var app = express();
 
@@ -25,7 +25,6 @@ app.use(session({ secret: 'definitely not a coke smuggling app' }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
