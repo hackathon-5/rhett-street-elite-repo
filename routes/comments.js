4b8ae87b704c06331bd1ab78fa7f647927baad2c
Clay Johnson <cjohnson@m33i.com>
2015-08-29 09:26:35 -0400

    borked up carrier rate stuff

--- routes/comments.js
+++ routes/comments.js
@@ -42,15 +42,31 @@ router.get('/list/:carrier', function(req, res, next) {
 
 /* delete comments */
 router.delete('/:id', function(req, res, next) {
-  Comment.findById(req.params.id, function(err, comment) {
-    if (err)
-      res.send(err);
-    comment.remove(function(err) {
-      if (err)
-        res.send(err);
-      res.json({ "success": true });
+    Comment.findById(req.params.id, function(err, comment) {
+        if (err) {
+            res.send(err);
+        }
+        Carrier.findOne({_id: comment.carrierId}, function(err, carrier) {
+            if (err) {
+                res.send(err);
+            }
+            carrier.totalComments -= 1;
+            carrier.totalRating -= parseInt(comment.rating);
+            carrier.averageRating = carrier.totalRating / carrier.totalComments;
+            carrier.save(function(err) {
+                if(err) {
+                    res.send(err);
+                }
+            });
+        });
+        comment.remove(function(err) {
+            if (err) {
+                res.send(err);
+            }
+
+            res.json({ "success": true });
+        });
     });
-  });
 });
 
 module.exports = router;
