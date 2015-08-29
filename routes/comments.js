82cd5b000f72958eb7f6313839462fdc6dda31c9
Charles Snyder <charlie.snyder@m33integrated.com>
2015-08-29 02:13:45 -0400

    have you ever had a dream where you had you cou you ha where you aa have dear  popcorn you had i have you you you could do anything?

--- routes/comments.js
+++ routes/comments.js
@@ -2,18 +2,44 @@ var express = require('express');
 var router = express.Router();
 var User = require('../models/user.js');
 var Carrier = require('../models/carrier.js');
+var Comment = require('../models/comment.js');
 
-/* login user */
-router.post('/login', function(req, res, next) {
-    User.findOne({ email: req.params.email}, function(err, user) {
+/* create comment */
+router.post('/', function(req, res, next) {
+  var comment = new Comment();
+  comment.userName = req.body.name;
+  comment.text = req.body.text;
+  comment.rating = req.body.rating;
+  comment.carrierId = req.body.carrierId;
+  comment.userId = req.body.userId;
+
+  comment.save(function(err) {
     if (err)
       res.send(err);
-    if (user.password == res.body.password) {
-      req.session.user = user;
+    res.json(comment);
+  });
+});
+
+
+/* get all comments for a carrier */
+router.get('/list/:carrierId', function(req, res, next) {
+  Comment.find({ carrierId: req.params.carrierId }, function(err, comments) {
+    if (err)
+      res.send(err);
+    res.json(comments);
+  });
+});
+
+/* delete comments */
+router.delete('/:id', function(req, res, next) {
+  Comment.findById(req.params.id, function(err, comment) {
+    if (err)
+      res.send(err);
+    comment.remove(function(err) {
+      if (err)
+        res.send(err);
       res.json({ "success": true });
-    } else {
-      res.json({ "success": false });
-    }
+    });
   });
 });
 
