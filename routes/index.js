be41227852a9ce94d35afedc8e93a5186dcb78d2
Ethan McGee <ethan.mcgee@m33integrated.com>
2015-08-29 00:12:51 -0400

    In the bonny blue highlands where the bagpipes play, lives the creature called Nessie, but it wasn't always this way. Before her great loch had ever been seen, shy Nessie lived life on the moors of Glenking. She was rumored a monster, which wasn't quite right, for she was really a softie with no bark and no bite. And with a wee friend that she had, the kind loyal and true... [squeaks] they played hide and seek in a pond just for two. Until one gloomy gray day at a quarter past two, from some distant dark land came a tartan-clad crew. With no time to waste and a toot on a bugle, no expenses were spared for tycoon McFroogal. This place is perfection. It's just what I need, with a few slight corrections, it'll be heaven, indeed.
    Her small protests fell on naught but deaf ears.
    Brilliant, sir. Brilliant.
    
    Nessie's anger burned bright.
    Her eyes flickered with tears.
    Then a friend scolded and told her...
    Keep a stiff upper lip.
    
    Donnae not cry. Be a braveheart.
    
    Chin up now. Pip, pip!
    Nessie gritted her teeth
    and decided to pack her whole kit and caboodle,
    and her dear friend McQuack.
    Fore!
    She set off to search for a new pond they could surf,
    a place with no golf clubs and no Astroturf.
    [bleating]
    But every pond was empty, every well was dry.
    Every trough was spoke for,
    yet still she had to try.
    She traveled each road and sought out every route.
    But instead of finding home, poor Ness got the boot.
    And the reward for her efforts,
    it was always this quip...
    Only wee babies cry. Chin up, now.
    
    Pip, pi-- [coughs]
    Nessie finally pooped out,
    she'd reached the end of her rope.
    Where do you go when there is no more hope?
    [McQuack squeaks]
    She stifled a whimper and a lump in her throat.
    "Chin up now. Pip, pip," she recited by rote.
    What began as a trickle soon turned to a shower.
    Nessie cried and she cried. She cried by the hour.
    She cried cups and pitchers and bottles and buckets.
    She cried tubs and barrels, quintets and quintuplets.
    For days Nessie cried.
    Nay, for weeks on it went.
    Till one day she sniffed, her tears finally spent.
    [squeaks]
    In surprise she looked up
    at McQuack there afloat,
    dunking and a-bobbing in what looked like a moat!
    Nay, 'twas a lake. A great loch!
    Nessie gasped in delight, sure it was paradise,
    with no golf tee in sight!
    Nessie learned herself a lesson in that bright and happy day,
    and it bears worth repeating, no matter what folks may say.
    Donnae be afraid to cry.
    It really is okay.
    Sometimes it's through our tears
    we find a better way.
    As for Nessie and McQuack,
    they're still there, I guess,
    forever playing hide and seek all along Loch Ness.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { session: req.session });
+  res.render('index', {session: req.session});
+});
+
+router.get('/users', function(req, res, next) {
+  if(req.session.user && req.session.user == 'admin') {
+    res.render('user_index', {session: req.session});
+  } else {
+    res.redirect('/');
+  }
+});
+
+router.get('/users/:id', function(req, res, next) {
+  if(req.session.user && req.session.user == 'admin') {
+    res.render('user_edit', {session: req.session, id: req.params.id});
+  } else {
+    res.redirect('/');
+  }
 });
 
 module.exports = router;
