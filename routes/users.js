04e09e6acaf27cfd2dabe56d06bdb7e5ca76adec
Charles Snyder <charlie.snyder@m33integrated.com>
2015-08-28 23:58:46 -0400

    I love it when you break everything -- Clay

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,25 @@ var User = require('../models/user.js');
 var Carrier = require('../models/carrier.js')
 var Client = require('../models/client.js');
 
+/* login user */
+router.post('/login', function(req, res, next) {
+    User.findOne({ email: req.params.email}, function(err, user) {
+    if (err)
+      res.send(err);
+    if (user.password == res.body.password) {
+      req.session.user = user;
+      res.json({ "success": true });
+    } else {
+      res.json({ "success": false });
+    }
+  });
+});
+
+/* login user */
+router.get('/logout', function(req, res, next) {
+  req.session.user = null;
+  res.json({ "success": true });
+});
 
 router.get('/list', function(req, res, next) {
     User.find(function(err, users) {
@@ -42,44 +61,16 @@ router.post('/', function(req, res, next) {
 });
 
 /* Update user */
-router.put('/:id', function(req, res, next) {
+router.delete('/:id', function(req, res, next) {
   User.findById(req.params.id, function(err, user) {
     if (err)
     	res.send(err);
-    user.name = req.body.name;
-    user.email = req.body.email;
-    user.password = req.body.password;
-    user.role  = req.body.role;
-    user.carrierId = req.body.carrierId;
-    user.clientId = req.body.clientId;
-    user.save(function(err) {
+    user.delete(function(err) {
     if (err)
       res.send(err);
-    res.json(user);
+    res.json({ "success": true });
     });
   });
 });
-
-/* login user */
-router.post('/login', function(req, res, next) {
-    User.findOne({ email: req.params.email}, function(err, user) {
-    if (err)
-      res.send(err);
-    if (user.password == res.body.password) {
-      req.session.user = user;
-      res.json({ "success": true });
-    } else {
-      res.json({ "success": false });
-    }
-  });
-});
-
-/* login user */
-router.get('/logout', function(req, res, next) {
-  req.session.user = null;
-  res.json({ "success": true });
-});
-
-
 
 module.exports = router;
